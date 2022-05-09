65a59017380a46f3c019f2e548be6893abf33d0d
baris.gungor@etiya.com <baris.gungor@etiya.com>
2022-05-09 16:45:31 +0300

    yeni case

--- MinMaxPatika.java
+++ MinMaxPatika.java
@@ -3,24 +3,24 @@ import java.util.Scanner;
 public class MinMaxPatika {
     public static void main(String[] args) {
         int n, inputVal, i,minVal,maxVal;
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Kaç Sayı gireceksiniz? ");
-        n = sc.nextInt();
-        n--;
-        System.out.print("Sayı girin: ");
-        inputVal = sc.nextInt();
-        minVal=inputVal;
-        maxVal=inputVal;
-        for (i = 0; i < n; i++) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Kaç Sayı gireceksiniz? ");
+            n = sc.nextInt();
+            n--;
             System.out.print("Sayı girin: ");
             inputVal = sc.nextInt();
-            if(inputVal<minVal){
-                minVal=inputVal;
-            }else if(inputVal>maxVal){
-                maxVal=inputVal;
+            minVal=inputVal;
+            maxVal=inputVal;
+            for (i = 0; i < n; i++) {
+                System.out.print("Sayı girin: ");
+                inputVal = sc.nextInt();
+                if(inputVal<minVal){
+                    minVal=inputVal;
+                }else if(inputVal>maxVal){
+                    maxVal=inputVal;
+                }
             }
         }
-
         System.out.println("minVal: "+minVal);
         System.out.println("maxVal: "+maxVal);
 
